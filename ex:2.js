1347d8e3e38f2c7d52a5e355d64369fdab0ddcc9
Mariana <101410421+Placito@users.noreply.github.com>
2023-11-23 14:21:06 +0000

    ex.2

---
+++ ex:2.js
@@ -0,0 +1,25 @@
+//firsat soluction
+const checkSum = function(matrix) {
+    const newArray = [];
+
+    matrix.map((itemArray) => {
+        let max = itemArray[0];
+        let min = itemArray[0];
+
+        itemArray.map((item) => {
+            if (item > max) max = item;
+            if (item < min) min = item;
+        });
+        newArray.push(max - min);
+    })
+
+    /*The reduce() method is an iterative method. It runs a "reducer" callback function over all elements in the array, in ascending-index order, and accumulates them into a single value.*/ 
+    return newArray.reduce((accumulator, a) => accumulator + a);
+}
+
+//second soluction
+const checksum = function(matrix) {
+    return matrix
+        .map((row) => Math.max(...row) - Math.min(...row))
+        .reduce((checksum, current) => checksum + current);
+}
